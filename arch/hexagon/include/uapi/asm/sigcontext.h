dc4e63912e5201a749d7ae63529c6030fe9b3cc3
Richard Kuo <rkuo@codeaurora.org>
2015-10-23 13:37:48 -0500

    Hexagon: don't use __aligned macro in exported headers
    Signed-off-by: Richard Kuo <rkuo@codeaurora.org>

--- arch/hexagon/include/uapi/asm/sigcontext.h
+++ arch/hexagon/include/uapi/asm/sigcontext.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2011, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2010-2011,2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -28,6 +28,6 @@
  */
 struct sigcontext {
 	struct user_regs_struct sc_regs;
-} __aligned(8);
+} __attribute__((aligned(8)));
 
 #endif
